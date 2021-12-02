912c6f2e1e1e713b046674109fef07a59b2f2a04
James Peilow <james@jamespeilow.com>
2021-12-02 20:50:13 +0000

    FEATURE: Added and styled NProgress

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -3,7 +3,15 @@
  * -----------------------------------------------------------------------------
  * Top level component that contains the page layout component.
  */
+import NProgress from 'nprogress';
+import Router from 'next/router';
 import Page from '../components/Page';
+
+import '../components/styles/nprogress.css';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 export default function MyApp({ Component, pageProps }) {
   return (
